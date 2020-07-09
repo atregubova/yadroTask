ec1e51b0aa20bfe6d7ef12afdc76da4f95faf2ed
atregubova <a.tregubova@list.ru>
2020-07-09 22:32:32 +0300

    eighth commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,14 +43,6 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
-import sphinx_rtd_theme
-
-extensions = [
-    ...
-    "sphinx_rtd_theme",
-]
-
-html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -77,7 +69,7 @@ language = u'ru'
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -85,7 +77,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
