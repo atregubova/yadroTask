d771617f30fd4ab460b75a5d269dc6e7c9c6c3ed
atregubova <a.tregubova@list.ru>
2020-07-09 22:22:26 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,15 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
+import sphinx_rtd_theme
+
+extensions = [
+    ...
+    "sphinx_rtd_theme",
+]
+
+html_theme = "sphinx_rtd_theme"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
