c1d5c5a117cab675b89ca978b0d7bbf939a18055
Anastasia Tregubova <a.tregubova@list.ru>
2020-07-09 21:54:51 +0300

    fourth commit

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     entry_points={
-        'gui_scripts': ['yadroTest=yadroTest.yadroTest:MainPage',],
+        'gui_scripts': ['yadroTask=yadroTask.yadroTask:MainPage',],
     },
 )
